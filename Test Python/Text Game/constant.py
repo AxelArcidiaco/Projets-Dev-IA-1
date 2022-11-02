98017131024112fb50e7f0a0c3d7ccae84be63d0
Axel Arcidiaco <73099105+AxelArcidiaco@users.noreply.github.com>
2022-11-02 15:37:01 +0100

    Début codage jeux textuel en Python

---
+++ Test Python/Text Game/constant.py
@@ -0,0 +1,16 @@
+# Constants for Earth
+EARTH_GRAV = 9.807 # in m/s²
+EARTH_MASS = 5.9722e24 # in kg
+EARTH_RADIUS = 6371 # in km
+
+# Constants for the Sun
+SOL_GRAV = 274 #in m/s²
+SOL_MASS = 1.98847e30 # in kg
+SOL_RADIUS = 696340 # in km
+SOL_LUMINOSITY = 1
+SOL_TEMP = 5778 # in Kelvin
+
+# Astronomical constants
+ASTRONOMICAL_UNIT = AU = 149597871 # in km
+LIGHT_SPEED = LS = 299792458 # in m/s
+LIGHT_YEAR = LY = 9460730472580800 # in km
