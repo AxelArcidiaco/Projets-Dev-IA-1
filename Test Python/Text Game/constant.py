4954fc8f088606c5ffcaac5db033c2df8a46459f
Axel Arcidiaco <73099105+AxelArcidiaco@users.noreply.github.com>
2022-11-03 10:57:51 +0100

    maj fichier constant.py

--- Test Python/Text Game/constant.py
+++ Test Python/Text Game/constant.py
@@ -1,71 +1,103 @@
-# A list of constants for the game
-
-# Constants for Earth
-EARTH_GRAV = EG = 9.807 # in m/s²
-EARTH_MASS = EM = 5.9722e24 # in kg
-EARTH_RADIUS = ER  = 6371 # in km
-
-# Constants for Luna / the Moon
-LUNA_GRAV = LG = 1.62 # in m/s²
-LUNA_MASS = LM = 7.34767309e22 # in kg
-LUNA_RADIUS = LR = 1737.4 # in km
-
-# Constants for Jupiter
-JUPITER_GRAV = JG = 24.79 # in m/s²
-JUPITER_MASS = JM = 1.898e27 # in kg
-JUPITER_RADIUS = JR = 69911 # in km
-
-# Constants for the Sun
-SOL_GRAV = SG = 274 #in m/s²
-SOL_MASS = SM = 1.98847e30 # in kg
-SOL_RADIUS = SR = 696340 # in km
-SOL_LUMINOSITY = 1
-SOL_TEMP = 5778 # in Kelvin
-
-# Astronomical constants
-ASTRONOMICAL_UNIT = AU = 149597871 # in km
-LIGHT_SPEED = LS = 299792458 # in m/s
-LIGHT_YEAR = LY = 9460730472580800 # in km
-
-# Star Type according to the Hardvard Classification
-O_TYPE = "O-type star" # also known as O-type main-sequence star, Blue giant, and Blue supergiant
-B_TYPE = "B-type star" # also known as B-type main-sequence star, Blue giant, and Blue supergiant
-A_TYPE = "A-type star" # also known as A-type main-sequence star
-F_TYPE = "F-type star" # also known as F-type main-sequence star
-G_TYPE = "G-type star" # also known as G-type main-sequence star, Yellow supergiant, and Yellow hypergiant
-K_TYPE = "K-type star" # also known as K-type main-sequence star
-M_TYPE = "M-type star" # also known as M-type main-sequence star, Red dwarf, Red giant, and Red supergiant
-L_TYPE = T_TYPE = Y_TYPE = "Brown dwarf"
-D_TYPE = "White dwarf"
-
-# Star type temperature in Kelvin
-O_MIN_TEMP = 30000
-B_MAX_TEMP = 30000
-B_MIN_TEMP = 10000
-A_MAX_TEMP = 10000
-A_MIN_TEMP = 7500
-F_MAX_TEMP = 7500
-F_MIN_TEMP = 6000
-G_MAX_TEMP = 6000
-G_MIN_TEMP = 5200
-K_MAX_TEMP = 5200
-K_MIN_TEMP = 3700
-M_MAX_TEMP = 3700
-M_MIN_TEMP = 2400
-
-# Star type color
-O_COLOR = "Blue"
-B_COLOR = "Blue white"
-A_COLOR = "White"
-F_COLOR = "Yellow white"
-G_COLOR = "Yellow"
-K_COLOR = "Orange"
-M_COLOR = "Red"
-
-# Other type of stellar object not in the Harvard Classification
-N_TYPE = "Neutron Star"
-BD_TYPE = "Black dwarf"
-BH_TYPE = "Black hole"
-SBH_TYPE = "Supermassive black hole"
-Q_TYPE = "Quasar"
-PS_TYPE = "Proto-star"
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "@@ -1,71 +1,35 @@\n",
+    "# A list of constants for the game\n",
+    "\n",
+    "# Constants for Earth\n",
+    "EARTH_GRAV = EG = 9.807 # in m/s²\n",
+    "EARTH_MASS = EM = 5.9722e24 # in kg\n",
+    "EARTH_RADIUS = ER  = 6371 # in km\n",
+    "\n",
+    "# Constants for Luna / the Moon\n",
+    "LUNA_GRAV = LG = 1.62 # in m/s²\n",
+    "LUNA_MASS = LM = 7.34767309e22 # in kg\n",
+    "LUNA_RADIUS = LR = 1737.4 # in km\n",
+    "\n",
+    "# Constants for Jupiter\n",
+    "JUPITER_GRAV = JG = 24.79 # in m/s²\n",
+    "JUPITER_MASS = JM = 1.898e27 # in kg\n",
+    "JUPITER_RADIUS = JR = 69911 # in km\n",
+    "\n",
+    "# Constants for the Sun\n",
+    "SOL_GRAV = SG = 274 #in m/s²\n",
+    "SOL_MASS = SM = 1.98847e30 # in kg\n",
+    "SOL_RADIUS = SR = 696340 # in km\n",
+    "SOL_LUMINOSITY = 1\n",
+    "SOL_TEMP = 5778 # in Kelvin\n",
+    "\n",
+    "# Astronomical constants\n",
+    "ASTRONOMICAL_UNIT = AU = 149597871 # in km\n",
+    "LIGHT_SPEED = LS = 299792458 # in m/s\n",
+    "LIGHT_YEAR = LY = 9460730472580800 # in km\n",
+    "\n",
+    "# Star Type according to the Hardvard Classification\n",
+    "O_TYPE = \"O-type star\" # also known as O-type main-sequence star, Blue giant, and Blue supergiant\n",
+    "B_TYPE = \"B-type star\" # also known as B-type main-sequence star, Blue giant, and Blue supergiant\n",
+    "A_TYPE = \"A-type star\" # also known as A-type main-sequence star\n",
+    "F_TYPE = \"F-type star\" # also known as F-type main-sequence star\n",
+    "G_TYPE = \"G-type star\" # also known as G-type main-sequence star, Yellow supergiant, and Yellow hypergiant\n",
+    "K_TYPE = \"K-type star\" # also known as K-type main-sequence star\n",
+    "M_TYPE = \"M-type star\" # also known as M-type main-sequence star, Red dwarf, Red giant, and Red supergiant\n",
+    "L_TYPE = T_TYPE = Y_TYPE = \"Brown dwarf\"\n",
+    "D_TYPE = \"White dwarf\"\n",
+    "\n",
+    "# Star type temperature in Kelvin\n",
+    "O_MIN_TEMP = 30000\n",
+    "B_MAX_TEMP = 30000\n",
+    "B_MIN_TEMP = 10000\n",
+    "A_MAX_TEMP = 10000\n",
+    "A_MIN_TEMP = 7500\n",
+    "F_MAX_TEMP = 7500\n",
+    "F_MIN_TEMP = 6000\n",
+    "G_MAX_TEMP = 6000\n",
+    "G_MIN_TEMP = 5200\n",
+    "K_MAX_TEMP = 5200\n",
+    "K_MIN_TEMP = 3700\n",
+    "M_MAX_TEMP = 3700\n",
+    "M_MIN_TEMP = 2400\n",
+    "\n",
+    "# Star type color\n",
+    "O_COLOR = \"Blue\"\n",
+    "B_COLOR = \"Blue white\"\n",
+    "A_COLOR = \"White\"\n",
+    "F_COLOR = \"Yellow white\"\n",
+    "G_COLOR = \"Yellow\"\n",
+    "K_COLOR = \"Orange\"\n",
+    "M_COLOR = \"Red\"\n",
+    "\n",
+    "# Other type of stellar object not in the Harvard Classification\n",
+    "N_TYPE = \"Neutron Star\"\n",
+    "BD_TYPE = \"Black dwarf\"\n",
+    "BH_TYPE = \"Black hole\"\n",
+    "SBH_TYPE = \"Supermassive black hole\"\n",
+    "Q_TYPE = \"Quasar\"\n",
+    "PS_TYPE = \"Proto-star\""
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3.9.13 ('base')",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.9.13"
+  },
+  "orig_nbformat": 4,
+  "vscode": {
+   "interpreter": {
+    "hash": "700029e5f9c8992c55d37e3a404b0b432c68d401a3c06e21a848c27e97636ef3"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
