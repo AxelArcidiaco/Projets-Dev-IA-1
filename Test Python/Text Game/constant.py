574a2a3cdea4b99356a6336bcee3342f6d1f77e8
Axel Arcidiaco <73099105+AxelArcidiaco@users.noreply.github.com>
2022-11-03 14:02:43 +0100

    màj constant.py

--- Test Python/Text Game/constant.py
+++ Test Python/Text Game/constant.py
@@ -1,112 +1,73 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# A list of constants for the game\n",
-    "\n",
-    "# Constants for Earth\n",
-    "EARTH_GRAV = 9.807 # gravity in m/s²\n",
-    "EARTH_MASS = 5.9722e24 # mass in kg\n",
-    "EARTH_RADIUS = 6371 # radius in km\n",
-    "\n",
-    "# Constants for Luna / the Moon\n",
-    "LUNA_GRAV = 1.62 # gravity in m/s²\n",
-    "LUNA_MASS = 7.34767309e22 # mass in kg\n",
-    "LUNA_RADIUS = 1737.4 # radius in km\n",
-    "\n",
-    "# Constants for Jupiter\n",
-    "JUPITER_GRAV = 24.79 # gravity in m/s²\n",
-    "JUPITER_MASS = 1.898e27 # mass in kg\n",
-    "JUPITER_RADIUS = 69911 # radius in km\n",
-    "\n",
-    "# Constants for the Sun\n",
-    "SOL_GRAV = 274 # gravity in m/s²\n",
-    "SOL_MASS = 1.98847e30 # mass in kg\n",
-    "SOL_RADIUS = 696340 # radius in km\n",
-    "SOL_LUMINOSITY = 1\n",
-    "SOL_TEMP = 5778 # in Kelvin\n",
-    "\n",
-    "# Astronomical constants\n",
-    "ASTRONOMICAL_UNIT = 149597871 # in km\n",
-    "LIGHT_SPEED = 299792458 # in m/s\n",
-    "LIGHT_YEAR = 9460730472580800 # in km\n",
-    "\n",
-    "# Star Type according to the Hardvard Classification\n",
-    "O_TYPE = \"O-type star\" # also known as O-type main-sequence star, Blue giant, and Blue supergiant\n",
-    "B_TYPE = \"B-type star\" # also known as B-type main-sequence star, Blue giant, and Blue supergiant\n",
-    "A_TYPE = \"A-type star\" # also known as A-type main-sequence star\n",
-    "F_TYPE = \"F-type star\" # also known as F-type main-sequence star\n",
-    "G_TYPE = \"G-type star\" # also known as G-type main-sequence star, Yellow supergiant, and Yellow hypergiant\n",
-    "K_TYPE = \"K-type star\" # also known as K-type main-sequence star\n",
-    "M_TYPE = \"M-type star\" # also known as M-type main-sequence star, Red dwarf, Red giant, and Red supergiant\n",
-    "L_TYPE = \"Brown dwarf\"\n",
-    "T_TYPE = \"Brown dwarf\"\n",
-    "Y_TYPE = \"Brown dwarf\"\n",
-    "D_TYPE = \"White dwarf\"\n",
-    "\n",
-    "# Star type temperature in Kelvin\n",
-    "O_MIN_TEMP = 30000\n",
-    "B_MAX_TEMP = 30000\n",
-    "B_MIN_TEMP = 10000\n",
-    "A_MAX_TEMP = 10000\n",
-    "A_MIN_TEMP = 7500\n",
-    "F_MAX_TEMP = 7500\n",
-    "F_MIN_TEMP = 6000\n",
-    "G_MAX_TEMP = 6000\n",
-    "G_MIN_TEMP = 5200\n",
-    "K_MAX_TEMP = 5200\n",
-    "K_MIN_TEMP = 3700\n",
-    "M_MAX_TEMP = 3700\n",
-    "M_MIN_TEMP = 2400\n",
-    "\n",
-    "# Star type color\n",
-    "O_COLOR = \"Blue\"\n",
-    "B_COLOR = \"Blue white\"\n",
-    "A_COLOR = \"White\"\n",
-    "F_COLOR = \"Yellow white\"\n",
-    "G_COLOR = \"Yellow\"\n",
-    "K_COLOR = \"Orange\"\n",
-    "M_COLOR = \"Red\"\n",
-    "\n",
-    "# Other type of stellar object not in the Harvard Classification\n",
-    "N_TYPE = \"Neutron Star\"\n",
-    "BD_TYPE = \"Black dwarf\"\n",
-    "BH_TYPE = \"Black hole\"\n",
-    "SBH_TYPE = \"Supermassive black hole\"\n",
-    "Q_TYPE = \"Quasar\"\n",
-    "PS_TYPE = \"Proto-star\""
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3.9.13 ('base')",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.13"
-  },
-  "orig_nbformat": 4,
-  "vscode": {
-   "interpreter": {
-    "hash": "700029e5f9c8992c55d37e3a404b0b432c68d401a3c06e21a848c27e97636ef3"
-   }
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+# A list of constants for the game
+
+# Constants for Earth
+EARTH_GRAV = 9.807 # gravity in m/s²
+EARTH_MASS = 5.9722e24 # mass in kg
+EARTH_RADIUS = 6371 # radius in km
+
+# Constants for Luna / the Moon
+LUNA_GRAV = 1.62 # gravity in m/s²
+LUNA_MASS = 7.34767309e22 # mass in kg
+LUNA_RADIUS = 1737.4 # radius in km
+
+# Constants for Jupiter
+JUPITER_GRAV = 24.79 # gravity in m/s²
+JUPITER_MASS = 1.898e27 # mass in kg
+JUPITER_RADIUS = 69911 # radius in km
+
+# Constants for the Sun
+SOL_GRAV = 274 # gravity in m/s²
+SOL_MASS = 1.98847e30 # mass in kg
+SOL_RADIUS = 696340 # radius in km
+SOL_LUMINOSITY = 1
+SOL_TEMP = 5778 # in Kelvin
+
+# Astronomical constants
+ASTRONOMICAL_UNIT = 149597871 # in km
+LIGHT_SPEED = 299792458 # in m/s
+LIGHT_YEAR = 9460730472580800 # in km
+
+# Star Type according to the Hardvard Classification
+O_TYPE = "O-type star" # also known as O-type main-sequence star, Blue giant, and Blue supergiant
+B_TYPE = "B-type star" # also known as B-type main-sequence star, Blue giant, and Blue supergiant
+A_TYPE = "A-type star" # also known as A-type main-sequence star
+F_TYPE = "F-type star" # also known as F-type main-sequence star
+G_TYPE = "G-type star" # also known as G-type main-sequence star, Yellow supergiant, and Yellow hypergiant
+K_TYPE = "K-type star" # also known as K-type main-sequence star
+M_TYPE = "M-type star" # also known as M-type main-sequence star, Red dwarf, Red giant, and Red supergiant
+L_TYPE = "Brown dwarf"
+T_TYPE = "Brown dwarf"
+Y_TYPE = "Brown dwarf"
+D_TYPE = "White dwarf"
+
+# Star type temperature in Kelvin
+O_MIN_TEMP = 30000
+B_MAX_TEMP = 30000
+B_MIN_TEMP = 10000
+A_MAX_TEMP = 10000
+A_MIN_TEMP = 7500
+F_MAX_TEMP = 7500
+F_MIN_TEMP = 6000
+G_MAX_TEMP = 6000
+G_MIN_TEMP = 5200
+K_MAX_TEMP = 5200
+K_MIN_TEMP = 3700
+M_MAX_TEMP = 3700
+M_MIN_TEMP = 2400
+
+# Star type color
+O_COLOR = "Blue"
+B_COLOR = "Blue white"
+A_COLOR = "White"
+F_COLOR = "Yellow white"
+G_COLOR = "Yellow"
+K_COLOR = "Orange"
+M_COLOR = "Red"
+
+# Other type of stellar object not in the Harvard Classification
+N_TYPE = "Neutron Star"
+BD_TYPE = "Black dwarf"
+BH_TYPE = "Black hole"
+SBH_TYPE = "Supermassive black hole"
+Q_TYPE = "Quasar"
+PS_TYPE = "Proto-star"
