a6dad98271862f06d3272a52c39c3f6a3dd43e1d
Axel Arcidiaco <73099105+AxelArcidiaco@users.noreply.github.com>
2022-11-03 14:14:53 +0100

    creation class file spaceship.py

---
+++ Test Python/Text Game/Classes/spaceship.py
@@ -0,0 +1,19 @@
+# Class spaceship defines the spaceship that is controlled by the player.
+from pickle import FALSE
+
+
+class spaceship:
+    colonists = 0 # Number of colonists aboard the spaceship
+    probes = 0 # Number of probes aboard the spaceship
+    techbase_state = 100 # State of the technological database inside the ship's computers
+    culturebase_state = 100 # State of the cultural database inside the ship's computers
+    warpdrive = FALSE # If a warp drive is present or not
+    
+    # Class constructor
+    def __init__(self, colonists, probes, warpdrive):
+        self.colonists = colonists
+        self.probes = probes
+        self.warpdrive = warpdrive
+        
+    
+    
