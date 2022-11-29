4904abb0781cb295f6a6fdc2e254a7af002a934c
Axel Arcidiaco <73099105+AxelArcidiaco@users.noreply.github.com>
2022-11-29 12:18:19 +0100

    added netflix mpd

--- Business Intelligence/Story 1/AxelArcidiaco-BI_Story_1-MPD.sql
+++ Business Intelligence/Story 1/AxelArcidiaco-BI_Story_1-MPD.sql
@@ -1,112 +1,69 @@
-CREATE TABLE Movies(
-   id_movie INT,
-   original_title VARCHAR(50) NOT NULL,
-   title VARCHAR(50),
-   runtime TIME NOT NULL,
-   original_language TEXT NOT NULL,
-   spoken_language TEXT,
-   adult LOGICAL NOT NULL,
-   budget VARCHAR(50),
-   overview TEXT,
-   release_date DATE NOT NULL,
-   PRIMARY KEY(id_movie)
+CREATE TABLE Individu(
+   code INT,
+   nom VARCHAR(25),
+   prenom VARCHAR(25),
+   PRIMARY KEY(code)
 );
 
-CREATE TABLE Countries(
-   id_country VARCHAR(50),
-   country_name VARCHAR(50) NOT NULL,
-   country_code VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_country)
+CREATE TABLE Professeur(
+   id_professeur INT,
+   competence VARCHAR(25),
+   code INT NOT NULL,
+   PRIMARY KEY(id_professeur),
+   UNIQUE(code),
+   FOREIGN KEY(code) REFERENCES Individu(code)
 );
 
-CREATE TABLE Genre(
-   id_genre INT,
-   genre_name VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_genre)
+CREATE TABLE Adresse(
+   id_adresse INT,
+   numero VARCHAR(10) NOT NULL,
+   rue TEXT NOT NULL,
+   ville VARCHAR(50) NOT NULL,
+   code_postal VARCHAR(5) NOT NULL,
+   PRIMARY KEY(id_adresse)
 );
 
-CREATE TABLE Persons(
-   id_person INT,
-   lastname VARCHAR(50) NOT NULL,
-   firstname VARCHAR(50) NOT NULL,
-   gender CHAR(1) NOT NULL,
-   PRIMARY KEY(id_person)
+CREATE TABLE Salle(
+   id_salle INT,
+   nom VARCHAR(25),
+   PRIMARY KEY(id_salle)
 );
 
-CREATE TABLE Characters(
-   id_character INT,
-   character_name VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_character)
+CREATE TABLE Matiere(
+   id_matiere INT,
+   nom VARCHAR(25),
+   PRIMARY KEY(id_matiere)
 );
 
-CREATE TABLE Studio(
-   id_studio INT,
-   studio_name VARCHAR(50),
-   PRIMARY KEY(id_studio)
+CREATE TABLE Etudiant(
+   id_etudiant INT,
+   diplome TEXT,
+   id_adresse INT NOT NULL,
+   code INT NOT NULL,
+   PRIMARY KEY(id_etudiant),
+   UNIQUE(id_adresse),
+   UNIQUE(code),
+   FOREIGN KEY(id_adresse) REFERENCES Adresse(id_adresse),
+   FOREIGN KEY(code) REFERENCES Individu(code)
 );
 
-CREATE TABLE Users(
-   id_user INT,
-   identifiant VARCHAR(50) NOT NULL,
-   password VARCHAR(50) NOT NULL,
-   id_person INT NOT NULL,
-   PRIMARY KEY(id_user),
-   UNIQUE(id_person),
-   FOREIGN KEY(id_person) REFERENCES Persons(id_person)
+CREATE TABLE Cours(
+   id_cours INT,
+   heure_de_debut TIME NOT NULL,
+   heure_de_fin TIME NOT NULL,
+   id_matiere INT NOT NULL,
+   id_salle INT NOT NULL,
+   id_professeur INT NOT NULL,
+   PRIMARY KEY(id_cours),
+   FOREIGN KEY(id_matiere) REFERENCES Matiere(id_matiere),
+   FOREIGN KEY(id_salle) REFERENCES Salle(id_salle),
+   FOREIGN KEY(id_professeur) REFERENCES Professeur(id_professeur)
 );
 
-CREATE TABLE Actors(
-   id_actor COUNTER,
-   id_person INT NOT NULL,
-   PRIMARY KEY(id_actor),
-   UNIQUE(id_person),
-   FOREIGN KEY(id_person) REFERENCES Persons(id_person)
-);
-
-CREATE TABLE Posséder(
-   id_movie INT,
-   id_genre INT,
-   PRIMARY KEY(id_movie, id_genre),
-   FOREIGN KEY(id_movie) REFERENCES Movies(id_movie),
-   FOREIGN KEY(id_genre) REFERENCES Genre(id_genre)
-);
-
-CREATE TABLE Tourner(
-   id_movie INT,
-   id_country VARCHAR(50),
-   PRIMARY KEY(id_movie, id_country),
-   FOREIGN KEY(id_movie) REFERENCES Movies(id_movie),
-   FOREIGN KEY(id_country) REFERENCES Countries(id_country)
-);
-
-CREATE TABLE Regarder(
-   id_user INT,
-   id_movie INT,
-   PRIMARY KEY(id_user, id_movie),
-   FOREIGN KEY(id_user) REFERENCES Users(id_user),
-   FOREIGN KEY(id_movie) REFERENCES Movies(id_movie)
-);
-
-CREATE TABLE Jouer(
-   id_actor INT,
-   id_character INT,
-   PRIMARY KEY(id_actor, id_character),
-   FOREIGN KEY(id_actor) REFERENCES Actors(id_actor),
-   FOREIGN KEY(id_character) REFERENCES Characters(id_character)
-);
-
-CREATE TABLE Appartenir(
-   id_movie INT,
-   id_character INT,
-   PRIMARY KEY(id_movie, id_character),
-   FOREIGN KEY(id_movie) REFERENCES Movies(id_movie),
-   FOREIGN KEY(id_character) REFERENCES Characters(id_character)
-);
-
-CREATE TABLE Produire(
-   id_movie INT,
-   id_studio INT,
-   PRIMARY KEY(id_movie, id_studio),
-   FOREIGN KEY(id_movie) REFERENCES Movies(id_movie),
-   FOREIGN KEY(id_studio) REFERENCES Studio(id_studio)
+CREATE TABLE suivre(
+   id_etudiant INT,
+   id_cours INT,
+   PRIMARY KEY(id_etudiant, id_cours),
+   FOREIGN KEY(id_etudiant) REFERENCES Etudiant(id_etudiant),
+   FOREIGN KEY(id_cours) REFERENCES Cours(id_cours)
 );
