af6818dcca3c01d26c57593149335f3190dfef2e
Axel Arcidiaco <73099105+AxelArcidiaco@users.noreply.github.com>
2022-12-05 13:21:26 +0100

    continued script dev

--- Business Intelligence/Story 3/AxelArcidiaco-Workshop SQL.sql
+++ Business Intelligence/Story 3/AxelArcidiaco-Workshop SQL.sql
@@ -93,3 +93,48 @@ INSERT INTO `utilisateur`(`nom`, `prenom`, `email`) VALUES
     'Emilie',
     'emilie@gmail.com'
 );
+
+-- Test d'insertion de plusieurs lignes dans la table aliment
+INSERT INTO `aliment`(
+    `nom`,
+    `marque`,
+    `sucre`,
+    `calories`,
+    `graisses`,
+    `proteines`,
+    `bio`
+)
+VALUES
+(
+    'poire',
+    'monoprix',
+    27.5,
+    134,
+    0.2,
+    1.1,
+    FALSE
+),(
+    'pomme',
+    'monoprix',
+    19.1,
+    72,
+    0.2,
+    0.4,
+    FALSE
+),(
+    'oeuf',
+    'carrefour',
+    0.6,
+    82,
+    5.8,
+    6.9,
+    TRUE
+),(
+    'lait d\'amande',
+    'bjorg',
+    4.5,
+    59,
+    3.9,
+    1.1,
+    TRUE
+);
