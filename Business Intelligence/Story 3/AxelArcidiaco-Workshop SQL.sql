c05f3d5f9d6c8fe0745ba3b1d1e8b5602b73dfde
Axel Arcidiaco <73099105+AxelArcidiaco@users.noreply.github.com>
2022-12-05 12:28:30 +0100

    continued script development

--- Business Intelligence/Story 3/AxelArcidiaco-Workshop SQL.sql
+++ Business Intelligence/Story 3/AxelArcidiaco-Workshop SQL.sql
@@ -73,3 +73,23 @@ VALUES(
     'Quentin',
     'quentin@gmail.com'
 );
+
+-- Test d'insertion de plusieurs nouvelles lignes dans la table utilisateur
+INSERT INTO `utilisateur`(`nom`, `prenom`, `email`) VALUES
+(
+    'Doe', 
+    'John', 
+    'john@yahoo.fr'
+),(
+    'Smith', 
+    'Jane', 
+    'jane@hotmail.com'
+),(
+    'Dupont',
+    'Sebastien',
+    'sebastien@orange.fr'
+),(
+    'Martin',
+    'Emilie',
+    'emilie@gmail.com'
+);
