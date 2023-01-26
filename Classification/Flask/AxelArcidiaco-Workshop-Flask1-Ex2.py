b99388cbcae3b8b61d9f7d34f69b1a3f58fabc30
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-01-26 15:50:21 +0100

    continued dev

---
+++ Classification/Flask/AxelArcidiaco-Workshop-Flask1-Ex2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("home.html", message = "Hello World!")
+
+#if __name__ == "__main__":
+#    app.run()
