431d5c363e9f37aee2d8731b543f7b9477470e87
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-01-26 16:12:52 +0100

    continued dev

--- Classification/Flask/AxelArcidiaco-Workshop-Flask1-Ex2.py
+++ Classification/Flask/AxelArcidiaco-Workshop-Flask1-Ex2.py
@@ -4,9 +4,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return render_template("home.html", message = "Hello World!")
+    return render_template("home.html", message="Hello World!")
+
 
 if __name__ == "__main__":
-   app.run()
+    app.run()
