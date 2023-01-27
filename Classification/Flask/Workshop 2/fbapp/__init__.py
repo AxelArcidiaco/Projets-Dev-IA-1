22f08a279b9d916718b2036543d7ca4dbe40e732
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-01-27 16:16:32 +0100

    continued dev

--- Classification/Flask/Workshop 2/fbapp/__init__.py
+++ Classification/Flask/Workshop 2/fbapp/__init__.py
@@ -4,3 +4,7 @@ from fbapp import models
 
 # Connect sqlalchemy to app
 models.db.init_app(app)
+
+@app.cli.command()
+def init_db():
+    models.init_db()
--- Classification/Flask/Workshop 2/fbapp/models.py
+++ Classification/Flask/Workshop 2/fbapp/models.py
@@ -1,6 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
-
 from fbapp.view import app
+import logging as lg
+import enum
+
+class Gender(enum.Enum):
+    female = 0
+    male = 1
+    other = 2
 
 # Create database connection object
 db = SQLAlchemy(app)
@@ -8,10 +14,18 @@ db = SQLAlchemy(app)
 class Content(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), nullable=False)
-    gender = db.Column(db.Integer(), nullable=False)
+    gender = db.Column(db.Enum(Gender), nullable=False)
 
     def __init__(self, description, gender):
         self.description = description
         self.gender = gender
 
 db.create_all()
+
+def init_db():
+    db.drop_all()
+    db.create_all()
+    db.session.add(Content("THIS IS SPARTAAAAAAA!!!", Gender['male']))
+    db.session.add(Content("What's your favorite scary movie?", Gender['female']))
+    db.session.commit()
+    lg.warning('Database initialized!')
