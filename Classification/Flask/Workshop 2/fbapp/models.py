4bfab1eac4cb2860ab5966521353249f90950125
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-01-27 15:29:03 +0100

    continued dev

---
+++ Classification/Flask/Workshop 2/fbapp/models.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from fbapp.view import app
+
+# Create database connection object
+db = SQLAlchemy(app)
+
+class Content(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(200), nullable=False)
+    gender = db.Column(db.Integer(), nullable=False)
+
+    def __init__(self, description, gender):
+        self.description = description
+        self.gender = gender
+
+db.create_all()
