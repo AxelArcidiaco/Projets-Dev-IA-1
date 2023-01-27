504cc2acb56e3b464b384f0f4755b5e1b7484a00
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-01-27 14:26:18 +0100

    continued dev

--- Classification/Flask/AxelArcidiaco-Workshop-Flask1-Ex3.py
+++ Classification/Flask/AxelArcidiaco-Workshop-Flask1-Ex3.py
@@ -12,5 +12,12 @@ def hello():
 def suite():
     return render_template("./templates/page_suivante.html")
 
+# gestion d'erreur 404
+@app.errorhandler(404)  
+# inbuilt function which takes error as parameter
+def not_found(e):  
+# defining function
+    return render_template("404.html")
+
 if __name__ == "__main__":
    app.run()
