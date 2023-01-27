6df53305b3c54ae8c408e73177b72591372131ec
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-01-27 14:45:43 +0100

    continued dev

---
+++ Classification/Flask/Workshop 1/AxelArcidiaco-Workshop1-Ex4.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home3.html")
+
+@app.route('/', methods=['POST'])
+def text_box():
+    text = request.form['text']
+    processed_text = text.upper()
+    return render_template("bienvenue.html" , message = processed_text )
+
+if __name__ == '__main__':
+   app.run()
