78425b8926d1f43bebc57d4d0ba54b2047908863
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-01-30 15:53:13 +0100

    continued dev

--- Classification/Flask/Workshop 2/fbapp/utils.py
+++ Classification/Flask/Workshop 2/fbapp/utils.py
@@ -1,6 +1,7 @@
 import random
+import os
 from fbapp.models import Content, Gender
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 
 
 def find_content(gender):
@@ -11,12 +12,32 @@ def find_content(gender):
 class OpenGraphImage:
 
     def __init__(self, first_name, description):
-      background = self.base()
-      background.show()
+        background = self.base()
+        background.show()
 
     def base(self):
-      img = Image.new('RGB', (1200, 630), '#18BC9C')
-      return img
+        img = Image.new('RGB', (1200, 630), '#18BC9C')
+        return img
+
+    def print_on_img(self, img, text, size, height):
+        # On commence par charger la police utilisée.
+        font = ImageFont.truetype(os.path.join(
+            'fbapp', 'static', 'fonts', 'Arcon-Regular.otf'), size)
+
+        # Création d'une nouvelle instance
+        draw = ImageDraw.Draw(img)
+
+        # textsize renvoie la largeur et la hauteur en pixel
+        # d'une chaine de caractères donnée.
+        w, h = draw.textsize(text, font)
+
+        # Calcul de la position pour que le texte soit centré
+        # et non pas aligné à gauche.
+        position = ((img.width - w) / 2, height)
+
+        # Ajout du texte à l'image.
+        draw.text(position, text, (255, 255, 255), font=font)
+
 
 description = """
     Toi, tu sais comment utiliser la console ! Jamais à court d'idées pour réaliser ton objectif, tu es déterminé-e et persévérant-e. Tes amis disent d'ailleurs volontiers que tu as du caractère et que tu ne te laisses pas marcher sur les pieds. Un peu hacker sur les bords, tu aimes trouver des solutions à tout problème. N'aurais-tu pas un petit problème d'autorité ? ;-)
