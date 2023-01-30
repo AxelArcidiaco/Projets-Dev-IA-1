f35342a071428f8c4501d71d13670c6af8988032
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-01-30 14:33:47 +0100

    continued dev

--- Classification/Flask/Workshop 2/fbapp/views.py
+++ Classification/Flask/Workshop 2/fbapp/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,12 @@ app.config.from_object('config')
 # To get one variable, tape app.config['MY_VARIABLE']
 
 @app.route('/')
-@app.route('/index/')
-def index():
-    return render_template('index.html')
+@app.route('/result/')
+def result():
+  description = """
+  Toi, tu n'as pas peur d'être seul ! Les grands espaces et les aventures sont faits pour toi. D'ailleurs,
+   Koh Lanta est ton émission préférée ! Bientôt tu partiras les cheveux au vent sur ton radeau.
+   Tu es aussi un idéaliste chevronné. Quelle chance !
+  """
+  return render_template('result.html', user_name='Axel', user_image=url_for('static', filename='tmp/cover_111823112767411.jpg'),
+                         description=description)
