4967b4bc0b90df32699b34022c5bbdd1e7571304
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-01-30 15:16:46 +0100

    continued dev

--- Classification/Flask/Workshop 2/fbapp/views.py
+++ Classification/Flask/Workshop 2/fbapp/views.py
@@ -1,3 +1,4 @@
+from .utils import find_content
 from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
@@ -6,7 +7,6 @@ app = Flask(__name__)
 app.config.from_object('config')
 # To get one variable, tape app.config['MY_VARIABLE']
 
-from .utils import find_content
 
 @app.route('/')
 @app.route('/index/')
@@ -15,10 +15,12 @@ def index():
         Toi, tu sais comment utiliser la console ! Jamais à court d'idées pour réaliser ton objectif, tu es déterminé-e et persévérant-e. Tes amis disent d'ailleurs volontiers que tu as du caractère et que tu ne te laisses pas marcher sur les pieds. Un peu hacker sur les bords, tu aimes trouver des solutions à tout problème. N'aurais-tu pas un petit problème d'autorité ? ;-)
     """
     return render_template('index.html',
-                            user_name='Axel',
-                            user_image=url_for('static', filename='img/profile.png'),
-                            description=description,
-                            blur=True)
+                           user_name='Axel',
+                           user_image=url_for(
+                               'static', filename='img/profile.png'),
+                           description=description,
+                           blur=True)
+
 
 @app.route('/result/')
 def result():
@@ -28,9 +30,9 @@ def result():
     profile_pic = 'http://graph.facebook.com/' + uid + '/picture?type=large'
     description = find_content(gender).description
     return render_template('result.html',
-                            user_name=user_name,
-                            user_image=profile_pic,
-                            description=description)
+                           user_name=user_name,
+                           user_image=profile_pic,
+                           description=description)
 
 
 # @app.route('/contents/<int:content_id>/')
