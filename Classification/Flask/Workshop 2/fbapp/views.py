bffe71c9f5507ae1908260ac921d7fe85cefd518
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-01-30 16:17:11 +0100

    Continued dev

--- Classification/Flask/Workshop 2/fbapp/views.py
+++ Classification/Flask/Workshop 2/fbapp/views.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, url_for, request
 from .utils import find_content, OpenGraphImage
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 # Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
 # To get one variable, tape app.config['MY_VARIABLE']
+
 
 @app.route('/')
 @app.route('/index/')
@@ -24,14 +25,16 @@ def index():
 
     og_description = "Découvre qui tu es vraiment en faisant le test ultime !"
     return render_template('index.html',
-                          user_name='Axel',
-                          user_image=url_for('static', filename='img/profile.png'),
-                          description=description,
-                          blur=True,
-                          page_title=page_title,
-                          og_url=og_url,
-                          og_image=og_image,
-                          og_description=og_description)
+                           user_name='Julio',
+                           user_image=url_for(
+                               'static', filename='img/profile.png'),
+                           description=description,
+                           blur=True,
+                           page_title=page_title,
+                           og_url=og_url,
+                           og_image=og_image,
+                           og_description=og_description)
+
 
 @app.route('/result/')
 def result():
@@ -40,14 +43,13 @@ def result():
     uid = request.args.get('id')
     profile_pic = 'http://graph.facebook.com/' + uid + '/picture?type=large'
     description = find_content(gender).description
-    img = 'tmp/sample.jpg'
-    og_url = url_for('index', img=img, _external=True)
     img = OpenGraphImage(uid, user_name, description).location
+    og_url = url_for('index', img=img, _external=True)
     return render_template('result.html',
-                            user_name=user_name,
-                            user_image=profile_pic,
-                            description=description,
-                            og_url=og_url)
+                           user_name=user_name,
+                           user_image=profile_pic,
+                           description=description,
+                           og_url=og_url)
 
 # @app.route('/contents/<int:content_id>/')
 # def content(content_id):
