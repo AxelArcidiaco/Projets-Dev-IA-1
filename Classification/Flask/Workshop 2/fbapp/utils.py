5dedc2e2bdfb9bdf355ea3411625253062746aed
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-01-30 16:08:34 +0100

    Continued dev

--- Classification/Flask/Workshop 2/fbapp/utils.py
+++ Classification/Flask/Workshop 2/fbapp/utils.py
@@ -16,9 +16,22 @@ class OpenGraphImage:
     def __init__(self, first_name, description):
         background = self.base()
         background.show()
-        # textwrap découpe une chaine de caractères sans couper les mots au milieu.
+        # textwrap découpe une chaine de caractères
+        # sans couper les mots au milieu.
         sentences = textwrap.wrap(description, width=60)
 
+        # current_h : Hauteur à laquelle commencer à écrire .
+        # pad : pixels à ajouter entre chaque ligne, en hauteur.
+        current_h, pad = 180, 10
+
+        for sentence in sentences:
+            w, h = self.print_on_img(background, sentence, 40, current_h)
+
+            # on incrémente la hauteur pour créer une nouvelle ligne
+            # en-dessous.
+            current_h += h + pad
+
+        background.show()
 
     def base(self):
         img = Image.new('RGB', (1200, 630), '#18BC9C')
@@ -42,6 +55,10 @@ class OpenGraphImage:
 
         # Ajout du texte à l'image.
         draw.text(position, text, (255, 255, 255), font=font)
+        
+        # Nous devons renvoyer la largeur et la hauteur
+        # pour imprimer le texte.
+        return (w, h)
 
 
 description = """
