bce668b701b2186bf152f4690db63a46f61712f4
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-01-30 15:12:50 +0100

    continued dev

--- Classification/Flask/Workshop 2/fbapp/views.py
+++ Classification/Flask/Workshop 2/fbapp/views.py
@@ -15,7 +15,7 @@ def index():
         Toi, tu sais comment utiliser la console ! Jamais à court d'idées pour réaliser ton objectif, tu es déterminé-e et persévérant-e. Tes amis disent d'ailleurs volontiers que tu as du caractère et que tu ne te laisses pas marcher sur les pieds. Un peu hacker sur les bords, tu aimes trouver des solutions à tout problème. N'aurais-tu pas un petit problème d'autorité ? ;-)
     """
     return render_template('index.html',
-                            user_name='Julio',
+                            user_name='Axel',
                             user_image=url_for('static', filename='img/profile.png'),
                             description=description,
                             blur=True)
