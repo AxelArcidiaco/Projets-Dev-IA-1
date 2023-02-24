e23e4c9ea176da92577b0dfe7c720b5185f50457
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-02-24 11:24:08 +0100

    started dev

---
+++ Regression & Clustering/Story 4 - API Rest/AxelArc - Workshop.py
@@ -0,0 +1,10 @@
+# Importation bibliothèque Python
+from flask import Flask
+from flask_restplus import Api, Resource
+
+persons = {
+    "Arcidiaco":"Axel",
+    "Perciot":"Nicolas",
+    "Panel":"Loic",
+    "Jacquenet":"Adrien"
+}
