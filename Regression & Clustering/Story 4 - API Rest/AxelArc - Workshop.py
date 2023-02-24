f9212da30e38cd844ad9c85d0a5703cd620a4d7a
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2023-02-24 11:44:04 +0100

    continued dev

--- Regression & Clustering/Story 4 - API Rest/AxelArc - Workshop.py
+++ Regression & Clustering/Story 4 - API Rest/AxelArc - Workshop.py
@@ -15,3 +15,14 @@ flask_app = Flask(__name__)
 app = Api(app=flask_app)
 
 name_space = app.namespace('main', description='Main APIs')
+
+@name_space.route("/")
+class MainClass(Resource):
+	def get(self):
+		return {
+			"status": "Got new data"
+		}
+	def post(self):
+		return {
+			"status": "Posted new data"
+		}
